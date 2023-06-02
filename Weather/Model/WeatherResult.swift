b123c3d682a882e0f8839280630638b52c7a63cf
Rohith Gurram <rohithiosdeveloper@gmail.com>
2023-06-01 20:24:10 -0700

    Handle missing/extra values from api result

--- Weather/Model/WeatherResult.swift
+++ Weather/Model/WeatherResult.swift
@@ -21,6 +21,26 @@ struct WeatherResult: Codable {
     let id: Int
     let name: String
     let cod: Int
+    let rain: Rain?
+    let snow: Snow?
+    
+    enum CodingKeys: String, CodingKey {
+        case coord
+        case weather
+        case base
+        case main
+        case visibility
+        case wind
+        case clouds
+        case dt
+        case sys
+        case timezone
+        case id
+        case name
+        case cod
+        case rain
+        case snow
+    }
 }
 
 struct Coord: Codable {
@@ -37,20 +57,60 @@ struct Weather: Codable {
 
 struct Main: Codable {
     let temp: Double
-    let temp_min: Double
-    let temp_max: Double
+    let tempMin: Double
+    let tempMax: Double
     let pressure: Int
     let humidity: Int
-    let feels_like: Double
+    let feelsLike: Double
+    let seaLevel: Int?
+    let grndLevel: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure
+        case humidity
+        case feelsLike = "feels_like"
+        case seaLevel = "sea_level"
+        case grndLevel = "grnd_level"
+    }
+}
+
+struct Rain: Codable {
+    let oneHour: Double?
+    let threeHour: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case oneHour = "1h"
+        case threeHour = "3h"
+    }
 }
 
 struct Wind: Codable {
     let speed: Double
     let deg: Int
+    let gust: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case speed
+        case deg
+        case gust
+    }
 }
 
 struct Clouds: Codable {
     let all: Int
+}
+
+struct Snow: Codable {
+    let oneHour: Double?
+    let threeHour: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case oneHour = "1h"
+        case threeHour = "3h"
+    }
 }
 
 struct Sys: Codable {
